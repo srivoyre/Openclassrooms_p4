b3b639aee482429428953d1ebda11aefed46a8b1
srivoyre <s.rivoyre@gmail.com>
2020-07-24 11:22:59 +0200

    Committing all forgotten changes before merging with remote

--- lib/Fram/Entity.php
+++ lib/Fram/Entity.php
@@ -1,7 +1,15 @@
 <?php
 namespace Fram;
 
-abstract class Entity
+abstract class Entity implements \ArrayAccess
 {
-    
+    protected $erreurs = [],
+              $id;
+    public function __construct(array $donnees = [])
+    {
+        if (empty($donnees) == false)
+        {
+            $this->hydrate($donnees));
+        }
+    }
 }
