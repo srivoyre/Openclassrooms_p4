8cd89cae386356e15aba1e444c09a47236a293f1
Sophie <s.rivoyre@gmail.com>
2020-07-29 14:18:13 +0200

    #34 Adding user role table in db

--- src/DAO/UserDAO.php
+++ src/DAO/UserDAO.php
@@ -8,8 +8,8 @@ class UserDAO extends DAO
 {
     public function register(Parameter $post)
     {
-        $sql = 'INSERT INTO user (pseudo, password, createdAt) VALUES (?,?,NOW())';
-        $this->createQuery($sql, [$post->get('pseudo'), password_hash($post->get('password'), PASSWORD_BCRYPT)]);
+        $sql = 'INSERT INTO user (pseudo, password, createdAt, role_id) VALUES (?,?,NOW(), ?)';
+        $this->createQuery($sql, [$post->get('pseudo'), password_hash($post->get('password'), PASSWORD_BCRYPT), 2]);
     }
 
     public function checkUser(Parameter $post)
