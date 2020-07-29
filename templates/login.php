2cc949f049478fc73a16f1d8fd29632d864c5369
Sophie <s.rivoyre@gmail.com>
2020-07-29 17:06:54 +0200

    #63 Adding verification methods to check if user is logged & user role

--- templates/login.php
+++ templates/login.php
@@ -4,6 +4,7 @@
 <p>En construction</p>
 
 <?= $this->session->show('error_login'); ?>
+<?= $this->session->show('need_login'); ?>
 
 <div>
     <form method="post" action="../public/index.php?route=login">
