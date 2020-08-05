df4408126f1bf8b3ef497bc43b95da47cee99dd9
Sophie <s.rivoyre@gmail.com>
2020-08-05 15:36:55 +0200

    #94 Adding navbar

--- src/view/base.php
+++ src/view/base.php
@@ -9,34 +9,51 @@
         <title>
             <?= $title ?>
         </title>
+        <!--jQuery, Poppers.js, Bootstrap JS, then custom script -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/js/bootstrap.min.js" integrity="sha384-XEerZL0cuoUbHE4nZReLT7nx9gQrQreJekYhJD9WNWhH8nEW+0c5qq7aIo2Wl30J" crossorigin="anonymous"></script>
         <script <?= $script ?> ></script>
+
     </head>
 
     <body>
 
         <header>
-            <a href="../public/index.php">Accueil</a>
-            <?php
-            if($this->session->get('loggedIn'))
-            {
-                if($this->session->get('user')->getIsAdmin()) {
-                ?>
-                <a href="../public/index.php?route=administration">Administration</a>
-                <?php
-                }
-            ?>
-                <a href="../public/index.php?route=profile">Profil</a>
-                <a href="../public/index.php?route=logout">Déconnexion</a>
-            <?php
-            }
-            else
-            {
-                ?>
-                <a href="../public/index.php?route=register">Inscription</a>
-                <a href="../public/index.php?route=login">Connexion</a>
-                <?php
-            }
-            ?>
+
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">Billet simple pour l'Alaska</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-link" href="../public/index.php">Accueil</a>
+                        <?php
+                        if($this->session->get('loggedIn'))
+                        {
+                            if($this->session->get('user')->getIsAdmin()) {
+                                ?>
+                                <a class="nav-link" href="../public/index.php?route=administration">Administration</a>
+                                <?php
+                            }
+                            ?>
+                            <a class="nav-link" href="../public/index.php?route=profile">Profil</a>
+                            <a class="nav-link" href="../public/index.php?route=logout">Déconnexion</a>
+                            <?php
+                        }
+                        else
+                        {
+                            ?>
+                            <a class="nav-link" href="../public/index.php?route=register">Inscription</a>
+                            <a class="nav-link" href="../public/index.php?route=login">Connexion</a>
+                            <?php
+                        }
+                        ?>
+                    </div>
+                </div>
+            </nav>
         </header>
 
         <div>
@@ -44,11 +61,11 @@
                 <?= $title ?>
             </h1>
 
-            <?php
-            //$previouspagehome = substr($_SERVER['HTTP_REFERER'], -9, 9) === 'index.php';
-            //$previouspagehome = substr($_SERVER['HTTP_REFERER'], -9, 9) === 'administration';
-            ?>
 
+        </div>
+
+
+        <div id="content">
             <?= $this->session->show('add_article'); ?>
             <?= $this->session->show('edit_article'); ?>
             <?= $this->session->show('delete_article'); ?>
@@ -65,10 +82,6 @@
             <?= $this->session->show('need_login'); ?>
             <?= $this->session->show('update_password'); ?>
             <?= $this->session->show('not_admin'); ?>
-
-        </div>
-
-        <div id="content">
 
             <?= $content ?>
         </div>
