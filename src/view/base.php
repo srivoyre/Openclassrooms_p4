dec17d0f291ea388e6ece3b8af705a19bcad4381
Sophie <s.rivoyre@gmail.com>
2020-08-05 15:17:25 +0200

    #94 Adding Bootstrap

--- src/view/base.php
+++ src/view/base.php
@@ -2,6 +2,10 @@
 <html lang="fr">
     <head>
         <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css" integrity="sha384-VCmXjywReHh4PwowAiWNagnWcLhlEJLA5buUprzK8rxFgeH0kww/aWY76TfkUoSX" crossorigin="anonymous">
+
         <title>
             <?= $title ?>
         </title>
