10e56c8ef9c34ad3b77c32856a6ecffca10f3357
Sophie <s.rivoyre@gmail.com>
2020-08-06 17:07:04 +0200

    #94 Updating password form responsive layout

--- src/view/update_password.php
+++ src/view/update_password.php
@@ -1,17 +1,36 @@
 <?php $this->title = 'Modifier mon mot de passe'; ?>
 
-<div>
-    <p>Le mot de passe de <span class="bold"><?= $this->session->get('user')->getPseudo(); ?></span> sera modifié</p>
-    <form method="post" action="../public/index.php?route=updatePassword">
-        <div class="form-group">
-            <label for="password">Nouveau mot de passe</label>
-            <br />
-            <input class="form-control" type="password" id="password" name="password">
-            <br />
-            <?= isset($errors['password']) ? $errors['password'] : ''; ?>
-        </div>
-        <input class="btn btn-primary"  type="submit" value="Mettre à jour" id="submit" name="submit">
-    </form>
+<div class="row my-2">
+    <div class="col-12 mx-0">
+        <a href="../public/index.php?route=profile">
+            << Retour au profil
+        </a>
+    </div>
 </div>
+<div class="row">
+    <div class="col-12">
+        <h1>Modifier mon mot de passe</h1>
+        <div>
+            Le mot de passe de <span class="bold"><?= $this->session->get('user')->getPseudo(); ?></span> sera modifié
+        </div>
 
-<a type="button" class="btn btn-secondary" href="../public/index.php?route=profile"><< Retour au profil</a>
+        <form method="post" action="../public/index.php?route=updatePassword">
+            <div class="form-group">
+                <div class="row">
+                    <div class="col-12">
+                        <label for="password">Nouveau mot de passe</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 col-md-8 pb-2">
+                        <input class="form-control" type="password" id="password" name="password"><?= isset($errors['password']) ? $errors['password'] : ''; ?>
+
+                    </div>
+                    <div class="col-12 col-md-4 pb-2">
+                        <input class="btn btn-primary"  type="submit" value="Mettre à jour" id="submit" name="submit">
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
