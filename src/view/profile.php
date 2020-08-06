d134641ad27baee1955aee9c0885293ebbfee895
Sophie <s.rivoyre@gmail.com>
2020-08-06 17:02:41 +0200

    #94 Profile responsive layout

--- src/view/profile.php
+++ src/view/profile.php
@@ -1,22 +1,39 @@
 <?php $this->title = 'Mon profil'; ?>
-
 <?php $loggedIn = isset($this->session) && $this->session->get('user')->getPseudo(); ?>
 
-<div>
-    <h2><?= $user->getPseudo(); ?></h2>
-    <p>Membre depuis le <?= $user->getCreatedAt(); ?></p>
-    <p>Nombre de commentaires en ligne : <?= $user->getNumberOfComments(); ?></p>
+<div class="row">
+    <div class="col-12">
+        <h1><?= $user->getPseudo(); ?></h1>
+        <p>Membre depuis le <?= $user->getCreatedAt(); ?></p>
+        <p>Nombre de commentaires en ligne : <?= $user->getNumberOfComments(); ?></p>
 
-    <form method="post" action="../public/index.php?route=updateEmail">
-        <div class="form-group">
-            <label for="email">E-mail</label>
-            <br />
-            <input class="form-control" type="email" id="email" name="email" value="<?= htmlspecialchars($user->getEmail()); ?>">
+        <form method="post" action="../public/index.php?route=updateEmail">
+            <div class="form-group">
+                <div class="row">
+                    <div class="col-12">
+                        <label for="email">E-mail</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 col-md-8 pb-2">
+                        <input class="form-control" type="email" id="email" name="email" value="<?= htmlspecialchars($user->getEmail()); ?>">
+                    </div>
+                    <div class="col-12 col-md-4 pb-2">
+                        <input class="btn btn-primary" type="submit" value="Mettre à jour mon e-mail" id="submitEmail" name="submitEmail">
+                    </div>
+                </div>
+
+            </div>
+        </form>
+        <div class="row mt-5">
+            <div class="col-6">
+                <a type="button" class="btn btn-outline-primary" href="../public/index.php?route=updatePassword">Modifier mon mot de passe</a>
+            </div>
+            <div class="col-6">
+                <a type="button" class="btn btn-outline-danger" href="../public/index.php?route=deleteAccount">Supprimer mon compte</a>
+            </div>
         </div>
-        <input class="btn btn-primary" type="submit" value="Mettre à jour mon e-mail" id="submitEmail" name="submitEmail">
-    </form>
+    </div>
 
-    <a type="button" class="btn btn-primary" href="../public/index.php?route=updatePassword">Modifier mon mot de passe</a>
-    <a type="button" class="btn btn-danger" href="../public/index.php?route=deleteAccount">Supprimer mon compte</a>
 </div>
-<a type="button" class="btn btn-secondary" href="../public/index.php">Retour à l'accueil</a>
+
