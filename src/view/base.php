9362e11ffb75518436093f8de6fe92f12e7b0685
Sophie <s.rivoyre@gmail.com>
2020-08-24 19:20:55 +0200

    #102 Adding metadata descriptions

--- src/view/base.php
+++ src/view/base.php
@@ -3,13 +3,34 @@
     <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, shrink-to-fit=no">
-
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css" integrity="sha384-VCmXjywReHh4PwowAiWNagnWcLhlEJLA5buUprzK8rxFgeH0kww/aWY76TfkUoSX" crossorigin="anonymous">
-        <link rel="stylesheet" href="../public/css/style.css">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
         <title>
             <?= $title ?>
         </title>
+
+        <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
+
+        <meta name="description" content="Billet simple pour l'Alaska : le nouveau roman de Jean Forteroche publié en ligne">
+        <meta name="author"     content="Sophie de Rivoyre">
+        <meta name="copyright"  content="MIT">
+
+        <meta property="og:type"        content="website">
+        <meta property="og:title"       content="Billet simple pour l'Alaska">
+        <meta property="og:url"         content="onlineprojects.fr">
+        <meta property="og:description" content="Billet simple pour l'Alaska : le nouveau roman de Jean Forteroche publié en ligne">
+        <meta property="og:image"       content="/img/logo.png">
+
+        <meta name="twitter:card"           content="summary">
+        <meta name="twitter:title"          content="Billet simple pour l'Alaska">
+        <meta name="twitter:site"           content="onlineprojects.fr">
+        <meta name="twitter:description"    content="Billet simple pour l'Alaska : le nouveau roman de Jean Forteroche publié en ligne">
+        <meta name="twitter:image"          content="../public/img/logo.png">
+        <meta name="twitter:creator"        content="@sophiederivoyre">
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css" integrity="sha384-VCmXjywReHh4PwowAiWNagnWcLhlEJLA5buUprzK8rxFgeH0kww/aWY76TfkUoSX" crossorigin="anonymous">
+        <link rel="stylesheet" href="../public/css/style.css">
+
         <!--jQuery, Poppers.js, Bootstrap JS, then custom scripts -->
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
@@ -25,32 +46,34 @@
         <header>
 
             <nav class="navbar navbar-expand-lg navbar-light bg-info">
-                <a class="navbar-brand text-white" href="../public/index.php">Billet simple pour l'Alaska</a>
+                <a class="navbar-brand text-white" href="index.php">
+                    <img alt="Billet simple pour l'Alaska" src="../img/logo.png" width="200"/>
+                </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
                     <div class="navbar-nav">
-                        <a class="nav-link text-white" href="../public/index.php">Tous les chapitres</a>
+                        <a class="nav-link text-white" href="index.php">Tous les chapitres</a>
                         <?php
                         if($this->session->get('loggedIn'))
                         {
                             if($this->session->get('user')->getIsAdmin()) {
                                 ?>
-                                <a class="nav-link text-white" href="../public/index.php?route=administration">Administration</a>
+                                <a class="nav-link text-white" href="index.php?route=administration">Administration</a>
                                 <?php
                             }
                             ?>
-                            <a class="nav-link text-white" href="../public/index.php?route=profile">Profil</a>
-                            <a class="nav-link text-white" href="../public/index.php?route=logout">Déconnexion</a>
+                            <a class="nav-link text-white" href="index.php?route=profile">Profil</a>
+                            <a class="nav-link text-white" href=".index.php?route=logout">Déconnexion</a>
                             <?php
                         }
                         else
                         {
                             ?>
-                            <a class="nav-link text-white" href="../public/index.php?route=register">Inscription</a>
-                            <a class="nav-link text-white" href="../public/index.php?route=login">Connexion</a>
+                            <a class="nav-link text-white" href="index.php?route=register">Inscription</a>
+                            <a class="nav-link text-white" href="index.php?route=login">Connexion</a>
                             <?php
                         }
                         ?>
